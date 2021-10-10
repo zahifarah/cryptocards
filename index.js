4535de95327602eeeb9dad8a1185ef25cd02e550
Zahi Farah <zahi.farah@protonmail.com>
2021-10-10 17:48:04 +0300

    Switch from individual fetch calls to a Promise.all() method.

--- index.js
+++ index.js
@@ -10,34 +10,28 @@ app.set("view engine", "ejs") // set ejs as view engine
 app.set("views", path.join(__dirname, "views"))
 app.use(express.static(path.join(__dirname, "public")))
 
-// const urls = [
-//   "https://api.cryptonator.com/api/ticker/btc-usd",
-//   "https://api.cryptonator.com/api/ticker/eth-usd",
-//   "https://api.cryptonator.com/api/ticker/ada-usd",
-// ];
-
-const btcUrl = "https://api.cryptonator.com/api/ticker/btc-usd"
-const ethUrl = "https://api.cryptonator.com/api/ticker/eth-usd"
-const adaUrl = "https://api.cryptonator.com/api/ticker/ada-usd"
+const urls = [
+  "https://api.cryptonator.com/api/ticker/btc-usd",
+  "https://api.cryptonator.com/api/ticker/eth-usd",
+  "https://api.cryptonator.com/api/ticker/ada-usd",
+]
 
 var btcUsdObj
 var ethUsdObj
 var adaUsdObj
 
-fetch(btcUrl)
-  .then((response) => response.json())
-  .then((data) => (btcUsdObj = data.ticker))
-  .then(() => console.log(btcUsdObj)) // for terminal feedback
-
-fetch(ethUrl)
-  .then((response) => response.json())
-  .then((data) => (ethUsdObj = data.ticker))
-  .then(() => console.log(ethUsdObj)) // for terminal feedback
-
-fetch(adaUrl)
-  .then((response) => response.json())
-  .then((data) => (adaUsdObj = data.ticker))
-  .then(() => console.log(adaUsdObj)) // for terminal feedback
+Promise.all(
+  urls.map((url) => {
+    return fetch(url).then((resp) => resp.json())
+  })
+)
+  .then((results) => {
+    btcUsdObj = results[0]
+    ethUsdObj = results[1]
+    adaUsdObj = results[2]
+  })
+  .then(() => console.log(btcUsdObj, ethUsdObj, adaUsdObj))
+  .catch(() => console.log("Errrrror!"))
 
 // home --> /cryptocards
 app.get("/cryptocards", (req, res) => {
